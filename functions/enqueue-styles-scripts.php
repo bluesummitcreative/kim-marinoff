a87a2cdaadb4da15a0b48cad7ceb3ff7921a0de0
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-08 12:56:30 -0600

    Update enqueue-styles-scripts.php

--- functions/enqueue-styles-scripts.php
+++ functions/enqueue-styles-scripts.php
@@ -2,6 +2,10 @@
 	//Enqueue Styles and Scripts
 	function bsc_enqueue_styles_scripts() {
 		wp_enqueue_style( 'css.css', get_template_directory_uri() . '/css/css.css'); // CSS
+		
+		wp_enqueue_script ('jquery'); 
+		wp_enqueue_script( 'core', get_template_directory_uri() . '/js/core.js', $deps, $ver, true);
+		wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', $deps, $ver, true);
   
   } 
 		
