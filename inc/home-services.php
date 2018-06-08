52f4b0e16b3b291c1814b0e18ee9b57963aa014b
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-08 14:30:35 -0600

    Update home-services.php

--- inc/home-services.php
+++ inc/home-services.php
@@ -1,28 +1,22 @@
 <section class="section section-lg bg-white">
     <div class="shell">
       <div class="range range-sm-center range-50">
-        <div class="cell-sm-5 cell-lg-3">
-          <div class="service text-center inset-lg-left-30 inset-lg-right-30">
-            <div class="icon-wrap"> <img src="/wp-content/themes/kim-marinoff/images/icon-wallet-45x45.png" alt="" class="img-responsive" width="45" height="45"> </div>
-            <h4> <a href="/services/">Franchisor Consulting</a> </h4>
-            <p>Are you a franchisor looking to grow your franchise? The Franchise Explorer can help in a number of ways.</p>
-          </div>
-        </div>
-        <div class="cell-sm-5 cell-lg-3">
+        
+        <div class="cell-sm-4">
           <div class="service text-center inset-lg-left-30 inset-lg-right-30">
             <div class="icon-wrap"> <img src="/wp-content/themes/kim-marinoff/images/icon-coffe-45x45.png" alt="" class="img-responsive" width="45" height="45"> </div>
             <h4> <a href="/services/">Franchise Brokering</a> </h4>
             <p>Our team of professional franchise consultants are working with quality candidates everyday exploring possible franchise opportunities.</p>
           </div>
         </div>
-        <div class="cell-sm-5 cell-lg-3">
+        <div class="cell-sm-4">
           <div class="service text-center inset-lg-left-30 inset-lg-right-30">
             <div class="icon-wrap"> <img src="/wp-content/themes/kim-marinoff/images/icon-business-45x45.png" alt="" class="img-responsive" width="45" height="45"> </div>
             <h4> <a href="/services/">Broker to Broker Marketing</a> </h4>
             <p>Our Broker to Broker marketing program re-introduces those efficiencies while we handle the large majority of the day-to-day tasks in dealing with large groups of brokers.</p>
           </div>
         </div>
-        <div class="cell-sm-5 cell-lg-3">
+        <div class="cell-sm-4">
           <div class="service text-center inset-lg-left-30 inset-lg-right-30">
             <div class="icon-wrap"> <img src="/wp-content/themes/kim-marinoff/images/icon-case-45x45.png" alt="" class="img-responsive" width="45" height="45"> </div>
             <h4> <a href="/services/">Franchise Marketing</a> </h4>
