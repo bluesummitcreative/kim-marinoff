c8449beff659e6509e43ba9f83efc843093d781b
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-08 14:32:17 -0600

    Update home-what-we-do-banner.php

--- inc/home-what-we-do-banner.php
+++ inc/home-what-we-do-banner.php
@@ -1,7 +1,7 @@
 <section class="section section-md bg-image bg-primary bg-fixed" style="background-image: url(/wp-content/themes/kim-marinoff/images/bg-02-1920x1200.jpg)">
     <div class="shell text-center text-lg-left">
-      <h2 class="reveal-inline-block text-middle">What We Do</h2>
+      <h2 class="reveal-inline-block text-middle">Franchisor Consulting</h2>
       <hr class="divider divider-vertical bg-white text-middle veil reveal-lg-inline-block">
-      <h4 class="text-light reveal-lg-inline-block text-middle inset-lg-right-60">We provide expert financial advice for businesses and individuals</h4>
-      <a href="our-services.html" class="button button-sm button-primary-filled text-middle">See All Services</a> </div>
+      <h4 class="text-light reveal-lg-inline-block text-middle inset-lg-right-60">Are you a franchisor looking to grow your franchise? The Franchise Explorer can help in a number of ways.</h4>
+      <a href="/services/" class="button button-sm button-primary-filled text-middle">Learn More</a> </div>
   </section>	
