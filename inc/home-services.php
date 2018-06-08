915cf822c6c6808146ca4e1e0c5fe8f2675e87ce
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-08 13:22:24 -0600

    Update home-services.php

--- inc/home-services.php
+++ inc/home-services.php
@@ -3,28 +3,28 @@
       <div class="range range-sm-center range-50">
         <div class="cell-sm-5 cell-lg-3">
           <div class="service text-center inset-lg-left-30 inset-lg-right-30">
-            <div class="icon-wrap"> <img src="images/icon-wallet-45x45.png" alt="" class="img-responsive" width="45" height="45"> </div>
+            <div class="icon-wrap"> <img src="/wp-content/themes/kim-marinoff/images/icon-wallet-45x45.png" alt="" class="img-responsive" width="45" height="45"> </div>
             <h4> <a href="our-services.html">Risk Management</a> </h4>
             <p>Risk management is intended to manage financial and other losses associated with risks to your assets or business.</p>
           </div>
         </div>
         <div class="cell-sm-5 cell-lg-3">
           <div class="service text-center inset-lg-left-30 inset-lg-right-30">
-            <div class="icon-wrap"> <img src="images/icon-coffe-45x45.png" alt="" class="img-responsive" width="45" height="45"> </div>
+            <div class="icon-wrap"> <img src="/wp-content/themes/kim-marinoff/images/icon-coffe-45x45.png" alt="" class="img-responsive" width="45" height="45"> </div>
             <h4> <a href="our-services.html">Retirement Planning</a> </h4>
             <p>Use our strategies and retirement advice, including retirement calculators to help you retire faster.</p>
           </div>
         </div>
         <div class="cell-sm-5 cell-lg-3">
           <div class="service text-center inset-lg-left-30 inset-lg-right-30">
-            <div class="icon-wrap"> <img src="images/icon-business-45x45.png" alt="" class="img-responsive" width="45" height="45"> </div>
+            <div class="icon-wrap"> <img src="/wp-content/themes/kim-marinoff/images/icon-business-45x45.png" alt="" class="img-responsive" width="45" height="45"> </div>
             <h4> <a href="our-services.html">Financial Planning</a> </h4>
             <p>We'll help you make sensible decisions about money that can help you achieve your goals in life.</p>
           </div>
         </div>
         <div class="cell-sm-5 cell-lg-3">
           <div class="service text-center inset-lg-left-30 inset-lg-right-30">
-            <div class="icon-wrap"> <img src="images/icon-case-45x45.png" alt="" class="img-responsive" width="45" height="45"> </div>
+            <div class="icon-wrap"> <img src="/wp-content/themes/kim-marinoff/images/icon-case-45x45.png" alt="" class="img-responsive" width="45" height="45"> </div>
             <h4> <a href="our-services.html">Taxation</a> </h4>
             <p>Tax planning considers the tax implications of business decisions, usually with the goal of minimizing tax liability.</p>
           </div>
