87ec9aa27978a8c034c5dfc2518c6b8fa6c3a201
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-08 13:22:34 -0600

    Update home-logo-slider.php

--- inc/home-logo-slider.php
+++ inc/home-logo-slider.php
@@ -1,12 +1,12 @@
 <section class="section section-md bg-alabaster">
     <div class="shell text-center">
       <div class="range range-30">
-        <div class="cell-xs-6 cell-sm-4 cell-lg-2"> <a href="#"> <img src="images/170x75.png" alt="" class="link-image" width="170" height="75"> </a> </div>
-        <div class="cell-xs-6 cell-sm-4 cell-lg-2"> <a href="#"> <img src="images/170x75.png" alt="" class="link-image" width="170" height="75"> </a> </div>
-        <div class="cell-xs-6 cell-sm-4 cell-lg-2"> <a href="#"> <img src="images/170x75.png" alt="" class="link-image" width="170" height="75"> </a> </div>
-        <div class="cell-xs-6 cell-sm-4 cell-lg-2"> <a href="#"> <img src="images/170x75.png" alt="" class="link-image" width="170" height="75"> </a> </div>
-        <div class="cell-xs-6 cell-sm-4 cell-lg-2"> <a href="#"> <img src="images/170x75.png" alt="" class="link-image" width="170" height="75"> </a> </div>
-        <div class="cell-xs-6 cell-sm-4 cell-lg-2"> <a href="#"> <img src="images/170x75.png" alt="" class="link-image" width="170" height="75"> </a> </div>
+        <div class="cell-xs-6 cell-sm-4 cell-lg-2"> <a href="#"> <img src="/wp-content/themes/kim-marinoff/images/170x75.png" alt="" class="link-image" width="170" height="75"> </a> </div>
+        <div class="cell-xs-6 cell-sm-4 cell-lg-2"> <a href="#"> <img src="/wp-content/themes/kim-marinoff/images/170x75.png" alt="" class="link-image" width="170" height="75"> </a> </div>
+        <div class="cell-xs-6 cell-sm-4 cell-lg-2"> <a href="#"> <img src="/wp-content/themes/kim-marinoff/images/170x75.png" alt="" class="link-image" width="170" height="75"> </a> </div>
+        <div class="cell-xs-6 cell-sm-4 cell-lg-2"> <a href="#"> <img src="/wp-content/themes/kim-marinoff/images/170x75.png" alt="" class="link-image" width="170" height="75"> </a> </div>
+        <div class="cell-xs-6 cell-sm-4 cell-lg-2"> <a href="#"> <img src="/wp-content/themes/kim-marinoff/images/170x75.png" alt="" class="link-image" width="170" height="75"> </a> </div>
+        <div class="cell-xs-6 cell-sm-4 cell-lg-2"> <a href="#"> <img src="/wp-content/themes/kim-marinoff/images/170x75.png" alt="" class="link-image" width="170" height="75"> </a> </div>
       </div>
     </div>
   </section>
