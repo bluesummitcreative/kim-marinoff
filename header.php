3cbe31f712001da008181d942d5c6208ed384530
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-08 14:15:02 -0600

    Update header.php

--- header.php
+++ header.php
@@ -1,19 +1,17 @@
-<!DOCTYPE html>
-<html class="wide wow-animation" lang="en">
 <head>
-<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1,maximum-scale=1,user-scalable=0">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta charset="utf-8">
-<?php wp_head(); ?>
-</head>
-<body>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
 <div class="page">
   <header class="rd-navbar-wrap">
     <nav data-layout="rd-navbar-fixed" data-focus-on-hover="false" data-sm-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-static" data-lg-stick-up="true" data-md-stick-up-offset="157px" data-lg-stick-up-offset="157px" class="rd-navbar rd-navbar-default">
       <div class="rd-navbar-inner">
         <div class="rd-navbar-panel">
           <button data-rd-navbar-toggle=".rd-navbar-nav-wrap" class="rd-navbar-toggle"> <span></span> </button>
-          <div class="brand rd-navbar-brand"> <a href="index.html"> <img class="img-responsive" src="/wp-content/uploads/logo-1.png" alt="" width="158" height="37"> </a> </div>
+          <div class="brand rd-navbar-brand"> <a href="<?php echo esc_url(home_url()); ?>"> <img class="img-responsive" src="/wp-content/uploads/logo-1.png" alt="" width="158" height="37"> </a> </div>
           <div class="rd-navbar-aside">
           	<div class="header_socials unit unit-horizontal "> <a target="_blank" href="#"><i class="fa fa-facebook"></i></a> <a target="_blank" href="#"><i class="fa fa-twitter"></i></a> <a target="_blank" href="#"><i class="fa fa-youtube"></i></a> </div>
             
@@ -21,7 +19,7 @@
         </div>
         <div class="rd-navbar-nav-wrap">
           <ul class="rd-navbar-nav">
-            <li class="active"> <a href="index.html">Home</a> </li>
+            <li> <a href="index.html">Home</a> </li>
             <li> <a href="about-us.html">About Us</a>
               <ul class="rd-navbar-dropdown">
                 <li> <a href="careers.html">Careers</a> </li>
