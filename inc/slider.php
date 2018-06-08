cf2ddc894cce4e7c067e616fdb69fa4fe311695e
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-08 13:58:56 -0600

    Update slider.php

--- inc/slider.php
+++ inc/slider.php
@@ -1,12 +1,12 @@
 <section data-min-height="500px" data-height="29.84%" class="swiper-container swiper-slider">
     <div class="swiper-wrapper">
 		<!--Start repeater here -->
-      <div class="swiper-slide" data-slide-bg="/wp-content/themes/kim-marinoff/images/home-slide-1.jpg">
+      <div class="swiper-slide" data-slide-bg="/wp-content/themes/kim-marinoff/images/home-slide-2.jpg">
         <div class="swiper-slide-caption">
           <section class="section-70">
             <div class="shell text-center text-md-left">
-              <h1 class="swiper-slide-title">We are the Consulting</h1>
-              <div class="group group-md"> <a class="button button-sm button-primary-filled veil reveal-sm-inline-block" href="about-us.html">Learn More</a> <a class="button button-sm button-default" href="our-services.html">Our Services</a> </div>
+              <h1 class="swiper-slide-title">Helping Others Find the Perfect Franchise</h1>
+              <div class="group group-md"> <a class="button button-sm button-primary-filled veil reveal-sm-inline-block" href="#">Learn More</a> <a class="button button-sm button-default" href="#">Our Services</a> </div>
             </div>
           </section>
         </div>
