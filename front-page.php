5b72c569afd82ddc18c86b274a039430f47c39a6
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-09 09:34:54 -0600

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -4,6 +4,6 @@
 	get_template_part('inc/home-services');
 	get_template_part('inc/home-what-we-do-banner');
 	get_template_part('inc/home-testimonials');
-	get_template_part('inc/home-logo-slider');
+	//get_template_part('inc/home-logo-slider');
 	get_footer(); 
 ?>
