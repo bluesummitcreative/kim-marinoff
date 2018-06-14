6900533396464347c391e2a2d5f71496862815eb
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-14 15:54:26 -0600

    Update home-intro.php

--- inc/home-intro.php
+++ inc/home-intro.php
@@ -1,11 +1,9 @@
 <section class="section bg-primary section-md">
     <div class="shell">
-      <h1>Kim Marinoff, The Franchise Explorer</h1>
-      <hr class="divider bg-primary">
-      <p>Welcome!  I’m  Kim  Marinoff,  The  Franchise  Explorer.</p>
-      <p>If  you’re  considering  a  franchise,  I’m  here  to  help.  For  nearly  two  decades,  I’ve  been  helping  my  clients  –  guiding  them  –  as  they  learn  about  the  world  of  franchising.  There  are  so  many  options,  there  is  so  much  information,  it  can  be  overwhelming.</p>
-      <p>I’m  here  to  help  you  learn  and  grow,  know  what  questions  to  ask,  and  what  to  pay  attention  to  when  considering  franchising,  and  when  selecting  the  right  franchise  for  you.</p>
-      <p>Here  on  my  site,  you’ll  find  information  to  guide  you  as  you  begin  the  process  of  exploring  franchise  opportunities,  including  my  Blog  and  other  valuable  resources.  <link  to  Blog  and  Offer/Ask> </p>
-      <p>You  can  also  learn  more  about  me  and  my  services,  and  how  to  get  in  touch  with  me  so  I  can  help  you  find  the  perfect  franchise.  <link  to  About  and  Contact>  </p>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <?php the_title( '<h1>', '</h1>' ); ?>
+        <hr class="divider bg-primary">
+        <?php the_content(); ?>
+      <?php endwhile; endif; ?>
     </div>
   </section>
