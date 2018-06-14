f3421ff9bf73bd1d29bc3c730edb26631821c890
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-14 16:36:02 -0600

    Update page-header.php

--- inc/page-header.php
+++ inc/page-header.php
@@ -1,6 +1,6 @@
 <section class="page-heading bg-image section section-sm bg-dark bg-fixed" style="background-image: url(images/1920x900.jpg)">
   <div class="shell text-center">
-	<h1 class="page-heading-title">PAGE HEADER TITLE</h1>
+	<h1 class="page-heading-title"><?php if ( is_home() ) { echo 'Blog'; /*} elseif ( is_404() ) { echo '404 Not Found';*/ } elseif ( is_search() ) { echo $wp_query->found_posts . ' results found for "' . esc_html( get_search_query( false ) ) .'"'; } else { the_title(); } ?></h1>
 	<hr class="divider veil reveal-sm-block">
   </div>
 </section>
