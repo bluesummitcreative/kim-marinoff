279f2d225e03415b8eb0a9a7eb060b46a2fc01ea
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-14 16:21:52 -0600

    Update enqueue-styles-scripts.php

--- functions/enqueue-styles-scripts.php
+++ functions/enqueue-styles-scripts.php
@@ -2,6 +2,10 @@
 	//Enqueue Styles and Scripts
 	function bsc_enqueue_styles_scripts() {
 		wp_enqueue_style( 'css.css', get_template_directory_uri() . '/css/css.css'); // CSS
+		wp_enqueue_style( 'font-awesome', '//use.fontawesome.com/releases/v5.0.13/css/all.css'); // Font Awesome
+		
+
+
 		
 		wp_enqueue_script ('jquery'); 
 		wp_enqueue_script( 'core.min', get_template_directory_uri() . '/js/core.min.js', $deps, $ver, true);
