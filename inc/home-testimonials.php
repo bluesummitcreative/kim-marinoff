5762ffd22c69b21f0d8ef91e481ecdf25e9e07b4
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-14 17:27:46 -0600

    Update home-testimonials.php

--- inc/home-testimonials.php
+++ inc/home-testimonials.php
@@ -4,50 +4,161 @@
       <hr class="divider bg-primary">
       <div class="owl-wrap inset-sm-left-70 inset-sm-right-70">
         <div data-items="1" data-sm-items="2" data-md-items="3" data-margin="29" data-dots-custom=".owl-custom-pagination" data-nav="true" data-dots="false" data-loop="false" data-dots-each="1" class="owl-carousel">
+          
           <div>
             <div class="quote-variant-1">
               <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
               <div class="quote-body">
-                <p> <q>Hi, guys! You've solved my problem in no time! Your efficient services and customer care are second to none!</q> </p>
+                <p> <q>I  so  much  appreciate  your  time  and  effort  to  help  me  leave  the  daily  burden  of  a  9  to  5  job.</q> </p>
               </div>
-              <div class="quote-meta"> <cite>Adam Watson</cite>
-                <p class="text-gray-light">(CEO &amp; Founder)</p>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div class="quote-variant-1">
-              <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
-              <div class="quote-body">
-                <p> <q>Wow, I'm so happy with your service. You managed to exceed my expectatio</q> </p>
-              </div>
-              <div class="quote-meta"> <cite>Louise Smith</cite>
-                <p class="text-gray-light">(CEO &amp; Founder)</p>
+              <div class="quote-meta"> <cite>Nancy  Goodwin </cite>
               </div>
             </div>
           </div>
-          <div>
-            <div class="quote-variant-1">
-              <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
-              <div class="quote-body">
-                <p> <q>Great organization!! Your prompt answer became a pleasant surprise for me.You've rendered an invaluable service! Thank you!</q> </p>
-              </div>
-              <div class="quote-meta"> <cite>Jack Walsh</cite>
-                <p class="text-gray-light">(CEO &amp; Founder)</p>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div class="quote-variant-1">
-              <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
-              <div class="quote-body">
-                <p> <q>Hi, guys! You've solved my problem in no time! Your efficient services and customer care are second to none!</q> </p>
-              </div>
-              <div class="quote-meta"> <cite>Adam Watson</cite>
-                <p class="text-gray-light">(CEO &amp; Founder)</p>
-              </div>
-            </div>
-          </div>
+            
+            <div>
+	<div class="quote-variant-1">
+	  <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
+	  <div class="quote-body">
+		<p> <q>With  extensive  knowledge  of  the  franchising  world,  Kim  has  what  it  takes  to  find  you  exactly  what  you're  looking  for  in  a  franchise.</q> </p>
+	  </div>
+	  <div class="quote-meta"> <cite>Dennis  Schooley</cite>
+	  </div>
+	</div>
+</div>
+
+<div>
+	<div class="quote-variant-1">
+	  <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
+	  <div class="quote-body">
+		<p> <q>With  Kim's  help  I  was  able  to  find  a  fantastic  franchise  opportunity  and  I  am  moving  forward  with  it.</q> </p>
+	  </div>
+	  <div class="quote-meta"> <cite> Glen  Hedstrom</cite>
+	  </div>
+	</div>
+</div>
+
+<div>
+	<div class="quote-variant-1">
+	  <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
+	  <div class="quote-body">
+		<p> <q>If  you  want  a  competent,  empowering  franchise  advocate,  then  Kim  is  your  best  bet.</q> </p>
+	  </div>
+	  <div class="quote-meta"> <cite>Emmett  Lombard</cite>
+	  </div>
+	</div>
+</div>
+
+<div>
+	<div class="quote-variant-1">
+	  <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
+	  <div class="quote-body">
+		<p> <q>Kim  has  been  a  wealth  of  knowledge  and  support.  Her  experience  and  work  ethic  are  second  to  none.</q> </p>
+	  </div>
+	  <div class="quote-meta"> <cite>Dave  Struck</cite>
+	  </div>
+	</div>
+</div> 
+
+<div>
+	<div class="quote-variant-1">
+	  <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
+	  <div class="quote-body">
+		<p> <q>I  recommend  Kim  for  anyone  looking  for  a  franchise  opportunity.</q> </p>
+	  </div>
+	  <div class="quote-meta"> <cite>Char Blackler</cite>
+	  </div>
+	</div>
+</div>
+
+<div>
+	<div class="quote-variant-1">
+	  <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
+	  <div class="quote-body">
+		<p> <q>I  recommend  Kim  for  anyone  looking  for  a  franchise  opportunity.</q> </p>
+	  </div>
+	  <div class="quote-meta"> <cite>Char Blackler</cite>
+	  </div>
+	</div>
+</div>
+
+<div>
+	<div class="quote-variant-1">
+	  <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
+	  <div class="quote-body">
+		<p> <q>I  highly  recommend  Kim  to  anyone  looking  to  take  the  next  step  in  their  financial  future  with  business  ownership  -  whether  in  franchising  or  not.</q> </p>
+	  </div>
+	  <div class="quote-meta"> <cite>Dan  Durney, CFE</cite>
+	  </div>
+	</div>
+</div>
+
+<div>
+	<div class="quote-variant-1">
+	  <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
+	  <div class="quote-body">
+		<p> <q>I  highly  recommend  anyone  who  is  looking  for  a  true  professional  in  the  franchise  industry  to  contact  Kim.</q> </p>
+	  </div>
+	  <div class="quote-meta"> <cite>Dennis  Stuver</cite>
+	  </div>
+	</div>
+</div>
+
+<div>
+	<div class="quote-variant-1">
+	  <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
+	  <div class="quote-body">
+		<p> <q>I  highly  recommend  Kim  as  a  franchising  expert  and  consultant.</q> </p>
+	  </div>
+	  <div class="quote-meta"> <cite>Brad  Shelton</cite>
+	  </div>
+	</div>
+</div>
+
+<div>
+	<div class="quote-variant-1">
+	  <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
+	  <div class="quote-body">
+		<p> <q>I  would  highly  recommend  Kim  to  anyone  considering  a  franchise  opportunity.</q> </p>
+	  </div>
+	  <div class="quote-meta"> <cite>Tricia  Charles</cite>
+	  </div>
+	</div>
+</div>
+
+<div>
+	<div class="quote-variant-1">
+	  <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
+	  <div class="quote-body">
+		<p> <q>I  highly  recommend  Kim  to  anyone  searching  for  a  new  opportunity  as  I'm  confident  that  she  will  take  you  carefully  down  the  path  of  discovery.</q> </p>
+	  </div>
+	  <div class="quote-meta"> <cite>Pam Duskocy</cite>
+	  </div>
+	</div>
+</div>
+
+<div>
+	<div class="quote-variant-1">
+	  <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
+	  <div class="quote-body">
+		<p> <q>Kim  is  a  great  listener  and  a  joy  to  work  with.  She  has  a  big  heart  and  loves  people.  She  is  passionate  about  helping  you  find  the  right  business.</q> </p>
+	  </div>
+	  <div class="quote-meta"> <cite>Walt Kailey</cite>
+	  </div>
+	</div>
+</div>
+
+<div>
+	<div class="quote-variant-1">
+	  <div class="quote-header"> <img src="/wp-content/themes/kim-marinoff/images/72x72.jpg" alt="" width="72" height="72"> </div>
+	  <div class="quote-body">
+		<p> <q>If  you  are  considering  owning  and  running  a  business  in  a  proven  or  growing  franchise  of  any  kind,  Kim  is  the  person  to  talk  to.</q> </p>
+	  </div>
+	  <div class="quote-meta"> <cite>Desmond Hardy</cite>
+	  </div>
+	</div>
+</div> 
+                     
         </div>
       </div>
       <div class="owl-controls-custom veil-sm">
