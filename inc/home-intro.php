8500d784cecb7239670216db177d26bde30e7e4a
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-14 15:59:33 -0600

    Update home-intro.php

--- inc/home-intro.php
+++ inc/home-intro.php
@@ -2,7 +2,7 @@
     <div class="shell">
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <?php the_title( '<h1>', '</h1>' ); ?>
-        <hr class="divider bg-primary">
+        
         <?php the_content(); ?>
       <?php endwhile; endif; ?>
     </div>
