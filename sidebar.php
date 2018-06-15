aca9810867e28104b25e3caeee3418166597e014
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-14 18:00:27 -0600

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -9,64 +9,12 @@
 		<div class="widget">
 		  <h4 class="text-bold">Categories</h4>
 		  <ul class="marked-list-bordered">
-			<li> <a href="#">Finance</a> </li>
-			<li> <a href="#">Insurance</a> </li>
-			<li> <a href="#">Franchising</a> </li>
-			<li> <a href="#">Funding trends</a> </li>
-			<li> <a href="#">Finance &amp; accounting</a> </li>
-			<li> <a href="#">Business plans</a> </li>
+			<?php wp_list_categories('title_li='); ?>
 		  </ul>
 		</div>
+		
 		<div class="widget">
-		  <h4 class="text-bold">Recent Posts</h4>
-		  <article class="post-preview">
-			<div class="unit unit-horizontal unit-spacing-sm">
-			  <div class="unit-left">
-				<div class="post-preview-image">
-				  <figure> <img src="images/70x70.jpg" alt="" width="70"> </figure>
-				</div>
-			  </div>
-			  <div class="unit-body">
-				<p class="post-preview-heading"> <a href="blog-post.html">Ease Your Way In ToThe Global Stock...</a> </p>
-				<div class="post-preview-meta">
-				  <time datetime="2016-02-04">Feb 4, 2016</time>
-				  <span>&nbsp;/&nbsp;4 Comments</span> </div>
-			  </div>
-			</div>
-		  </article>
-		  <article class="post-preview">
-			<div class="unit unit-horizontal unit-spacing-sm">
-			  <div class="unit-left">
-				<div class="post-preview-image">
-				  <figure> <img src="images/70x70.jpg" alt="" width="70"> </figure>
-				</div>
-			  </div>
-			  <div class="unit-body">
-				<p class="post-preview-heading"> <a href="blog-post.html">Making Cents Investments in Start-up...</a> </p>
-				<div class="post-preview-meta">
-				  <time datetime="2016-02-04">Feb 4, 2016</time>
-				  <span>&nbsp;/&nbsp;4 Comments</span> </div>
-			  </div>
-			</div>
-		  </article>
-		  <article class="post-preview">
-			<div class="unit unit-horizontal unit-spacing-sm">
-			  <div class="unit-left">
-				<div class="post-preview-image">
-				  <figure> <img src="images/70x70.jpg" alt="" width="70"> </figure>
-				</div>
-			  </div>
-			  <div class="unit-body">
-				<p class="post-preview-heading"> <a href="blog-post.html">Why Do I Need To Use Financial Consulting..</a> </p>
-				<div class="post-preview-meta">
-				  <time datetime="2016-02-04">Feb 4, 2016</time>
-				  <span>&nbsp;/&nbsp;4 Comments</span> </div>
-			  </div>
-			</div>
-		  </article>
-		</div>
-		<div class="widget">
-		  <div class="banner">
+		  <div class="banner text-center">
 			<h4 class="banner-title">Do You Need Help?</h4>
 			<p class="banner-desc">Contact us at the FinExpert office nearest to you or submit a business inquiry online.</p>
 			<a href="contacts.html" class="button button-sm banner-button">Contact Us</a> </div>
