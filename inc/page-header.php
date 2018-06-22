53246faf9fee6e1095d5ffe9b1bf5471c29a31d1
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-22 13:36:34 -0600

    Update page-header.php

--- inc/page-header.php
+++ inc/page-header.php
@@ -1,15 +1,6 @@
-<section class="page-heading bg-image section section-sm bg-dark bg-fixed" style="background-image: url(images/1920x900.jpg)">
+<section class="page-heading bg-image section section-sm bg-dark bg-fixed" style="background-image: url(/wp-content/themes/kim-marinoff/images/1920x900.jpg)">
   <div class="shell text-center">
 	<h1 class="page-heading-title"><?php if ( is_home() ) { echo 'Blog'; } elseif ( is_404() ) { echo '404 Not Found'; } elseif ( is_search() ) { echo $wp_query->found_posts . ' results found for "' . esc_html( get_search_query( false ) ) .'"'; } else { echo get_the_title(); } ?></h1>
 	<hr class="divider veil reveal-sm-block">
   </div>
 </section>
-<section>
-      <div class="shell">
-        <ul class="breadcrumbs">
-          <li> <a href="index.html">Home</a> </li>
-          <li class="active">Contacts</li>
-        </ul>
-      </div>
-      <hr class="hr">
-    </section>
