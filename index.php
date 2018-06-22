81accb85cf6c6ca7505ca6cbd1c51d934f3a6bd2
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-06-22 13:45:01 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php get_header(); ?>
 <main class="page-content">
     <?php get_template_part('inc/page-header'); ?>
-    <section class="section section-md bg-white">
+    	<section class="section section-md bg-white">
+     	    <div class="shell">
+		    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pellentesque, sapien id egestas blandit, mi purus laoreet neque, semper congue erat eros id nisi. Interdum et malesuada fames ac ante.</p>
+		    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pellentesque, sapien id egestas blandit, mi purus laoreet neque, semper congue erat eros id nisi. Interdum et malesuada fames ac ante.</p>
+		    
+	    </div>
+	</section>
+	 
       <div class="shell">
         <div class="range range-80">
           <?php
