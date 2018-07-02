3bdffb9f3ff058cf35811eadd8a3a901bfa85f0c
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-07-02 16:21:19 -0600

    Update videos.php

--- videos.php
+++ videos.php
@@ -9,22 +9,12 @@
 			<div class="range range-80">
 				<?php 
 					get_template_part('inc/loop');
-          $args = array(
-           'cat' => 3,
-           'posts_per_page' => -1
-             );
-             $the_query = new WP_Query( $args );
-             // The Loop
-             if ( $the_query->have_posts() ) { ?>
-             <ol>
-
-                <?php while ( $the_query->have_posts() ) {?>
-
-                   <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-                <?php } ?>
-                
-                </ol>
-             	<?php }
+					
+					query_posts('cat=5'); 
+					if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					   <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+					<?php endwhile; endif;
+					wp_reset_query();		
 					get_sidebar();
 				?>
 			</div>
