f8754ef801296e68dc1eb33ec083cbc14c8e0830
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-07-02 16:16:59 -0600

    Update videos.php

--- videos.php
+++ videos.php
@@ -21,7 +21,7 @@
                 <?php while ( $the_query->have_posts() ) {?>
 
                    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-                } 
+                <?php } ?>
                 
                 </ol>
              	<?php }
