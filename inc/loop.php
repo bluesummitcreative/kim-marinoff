6e6c75bc003dc1445facb2e2c26eee64a9c06db6
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-07-02 16:30:52 -0600

    Update loop.php

--- inc/loop.php
+++ inc/loop.php
@@ -3,5 +3,18 @@
 	if ( have_posts() ) : while ( have_posts() ) : the_post(); 
 		the_content();
 	endwhile; endif;
+	
+	if( is_page('20') {
+	   $catquery = new WP_Query( 'cat=3&posts_per_page=-1' ); ?>
+		<ol>
+		<?php while($catquery->have_posts()) : $catquery->the_post(); ?>
+
+		<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+		<?php endwhile; ?>
+		</ol>
+		<?php 
+		wp_reset_postdata();
+	}
+	else {}
 ?>
 </div>
