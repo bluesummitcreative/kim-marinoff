fea464978b52590e7d941a9aae2086e7a0cb0ca7
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-07-05 23:20:16 -0600

    Update home-services.php

--- inc/home-services.php
+++ inc/home-services.php
@@ -5,24 +5,22 @@
         <div class="cell-sm-4">
           <div class="service text-center inset-lg-left-30 inset-lg-right-30">
             <div class="icon-wrap"> <i class="fas fa-check"></i> </div>
-            <h4> <a href="/services/">Franchise Brokering</a> </h4>
-            <p>Our team of professional franchise consultants are working with quality candidates everyday exploring possible franchise opportunities.</p>
+            <h4> <a href="/services/">Matchmaking</a> </h4>
+            <p>With hundreds, even thousands of franchises to consider, you need someone with the skills and experience to quickly narrow your choices to a select few.</p>
           </div>
         </div>
         <div class="cell-sm-4">
           <div class="service text-center inset-lg-left-30 inset-lg-right-30">
             <div class="icon-wrap"> <i class="fas fa-users"></i> </div>
-            <h4> <a href="/services/">Broker-to-Broker Marketing</a> </h4>
-            <p>Our Broker-to-Broker marketing program re-introduces those efficiencies while we handle the large majority of the day-to-day tasks in dealing with large groups of brokers.</p>
+            <h4> <a href="/services/">Tools for Validation</a> </h4>
+            <p>This critical stage is the time to identify and analyze many important factors before making a final decision – a deep-dive examination of each franchise you’re considering.</p>
           </div>
         </div>
         <div class="cell-sm-4">
           <div class="service text-center inset-lg-left-30 inset-lg-right-30">
             <div class="icon-wrap"> <i class="fas fa-chart-line"></i> </div>
-            <h4> <a href="/services/">Franchise Marketing</a> </h4>
-            <p>We'll work with you to make effective advertising placements with detailed reporting to track your clicks, leads and franchise sales.
-
-</p>
+            <h4> <a href="/services/">Financing</a> </h4>
+            <p>What tools and funding sources are available to you? How do we create a plan to best finance your franchise investment?</p>
           </div>
         </div>
       </div>
