340d49d10230007c327526f534b0c8dc1e475449
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-07-07 07:27:06 -0600

    Update slider.php

--- inc/slider.php
+++ inc/slider.php
@@ -1,10 +1,10 @@
 <section data-min-height="500px" data-height="29.84%" class="swiper-container swiper-slider">
     <div class="swiper-wrapper">
 		<!--Start repeater here -->
-      <div class="swiper-slide" data-slide-bg="/wp-content/themes/kim-marinoff/images/home-slide-2.jpg">
+      <div class="swiper-slide" data-slide-bg="/wp-content/uploads/A2B-Header-1-2018-002.jpg">
         <div class="swiper-slide-caption">
           <section class="section-70">
-            <div class="shell text-center text-md-left">
+            <div class="shell text-center text-md-right">
               <h1 class="swiper-slide-title">Franchise Matchmaking Services</h1>
               <div class="group group-md"> <a class="button button-sm button-primary-filled veil reveal-sm-inline-block" href="/videos/">Learn More</a> <a class="button button-sm button-default" href="/services/">Our Services</a> </div>
             </div>
