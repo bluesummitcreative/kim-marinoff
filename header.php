6f9b4e289647f130cb05b0965a91cfdbc78d833b
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-07-11 12:23:02 -0600

    Update header.php

--- header.php
+++ header.php
@@ -4,6 +4,16 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-122164230-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-122164230-1');
+		</script>
+
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
