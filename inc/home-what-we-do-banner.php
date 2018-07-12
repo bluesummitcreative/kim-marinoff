03a900b25086fab915e633fecc0bf45fee095dc6
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-07-12 17:28:16 -0600

    Update home-what-we-do-banner.php

--- inc/home-what-we-do-banner.php
+++ inc/home-what-we-do-banner.php
@@ -1,6 +1,6 @@
 <section class="section section-md bg-image bg-primary bg-fixed" style="background-image: url(/wp-content/themes/kim-marinoff/images/bg-02-1920x1200.jpg)">
     <div class="shell text-center text-lg-left">
-      <h3 class="reveal-inline-block text-middle">Franchisor Consulting</h2>
+      <h3 class="reveal-inline-block text-middle">Prospective Franchisee Services</h2>
       <hr class="divider divider-vertical bg-white text-middle veil reveal-lg-inline-block">
       <h4 class="text-light reveal-lg-inline-block text-middle inset-lg-right-60">Are you a franchisor looking to grow your franchise?<br />A2B Franchise Consulting can help in a number of ways.</h4>
       <a href="/services/" class="button button-sm button-primary text-middle">Learn More</a> </div>
