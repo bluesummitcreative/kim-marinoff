d38ba73f673cf2763e495656b7390ae081bf363f
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-09-04 15:54:51 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
     <?php get_template_part('inc/page-header'); ?>
     	<section class="section section-md bg-white">
      	    <div class="shell">
-		    <p>Welcome to my blog! Here, I'll post about news from the world of franchises, address important industry topics in detail, and answer some of your questions. I hope you'll read and comment and contribute to the discussion.</p>
-		    <p>Have a question? Please ask, via the email listed here, or my Contact page on this site.</p>
+		    <?php the_field('blog_intro'); ?>
 		    
 	    </div>
 	</section>
