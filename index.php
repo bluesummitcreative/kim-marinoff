9e4c381b80234fc4ce61bf08d561d121f8156914
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-09-04 15:57:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
     <?php get_template_part('inc/page-header'); ?>
     	<section class="section section-md bg-white">
      	    <div class="shell">
-		    <?php the_field('blog_intro'); ?>
-		    
+		    <?php the_field('blog_intro', get_option('page_for_posts')); ?>	    
 	    </div>
 	</section>
 	 
